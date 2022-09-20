b7c3ce2693aa16133b473402a65844f3d773cd82
Fikasm <67736687+iFika@users.noreply.github.com>
2022-09-20 14:53:14 +0700

    Script untuk Menjalankan Menyuruh Mouse Arah bawah.

---
+++ DATA/load_donasi/Mouse Bawah.js
@@ -0,0 +1,8 @@
+//Yang Mau Keyboard Bindnya gini aja : Misal Mau Press G
+;(async () => {
+await mouse.move(down(150));
+
+// Dan Kalau Mouse Control :
+//await mouse.move(down(10000000000)); <-- Arah Mouse kebawah
+//await mouse.click(Button.LEFT); <-- Tiba-tiba Nembak Sendiri
+})()
