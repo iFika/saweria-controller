03042a6c41040abc8df31fc1ebf1191c3fff9735
Fikasm <67736687+iFika@users.noreply.github.com>
2022-09-20 14:52:39 +0700

    Script untuk menjalankan Drop Senjata.

---
+++ DATA/load_donasi/Drop Senjata.js
@@ -0,0 +1,8 @@
+//Yang Mau Keyboard Bindnya gini aja : Misal Mau Press G
+;(async () => {
+await keyboard.pressKey(Key.G)
+
+// Dan Kalau Mouse Control :
+//await mouse.move(down(10)); <-- Arah Mouse kebawah
+//await mouse.click(Button.LEFT); <-- Tiba-tiba Nembak Sendiri
+})()
